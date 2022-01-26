a344d0c6a3f416c56963dbf585a47ddb36fe5839
nengcham <nengcham7@gmail.com>
2022-01-26 14:50:24 +0900

    2022-01-26 14:50

--- java/com/example/demo/calc/CalcApp.java
+++ java/com/example/demo/calc/CalcApp.java
@@ -5,10 +5,13 @@ package com.example.demo.calc;
  * author     : choigeonil
  * date       : 2022-01-25
  * desc       : 숫자 2개와 연산자 받아서 결과를 리턴하는 앱
+ * variable   : [클래스변수] CALC_APP, [인스턴스변수] num1 opcode num2
+ *              [로컬변수] res, result, [파라미터] num1 opcode num2
  * ================================
  * DATE         AUTHOR        NOTE
  * ================================
  * 2022-01-24   choigeonil    최초 생성
+ * 2022-01-26   choigeonil    if절 작성, 4칙연산 추가
  */
 public class CalcApp {
     public static String CALC_APP = "계산기";
@@ -22,16 +25,21 @@ public class CalcApp {
         this.opcode = opcode;
         this.num2 = num2;
         int res = 0;
+
         if (opcode.equals("+")){
-            res = num1 + num2;
+            res = this.num1 + this.num2;
         } else if (opcode.equals("-")){
-            res = num1 - num2;
+            res = this.num1 - this.num2;
         } else if (opcode.equals("*")){
-            res = num1 * num2;
-        } else if (opcode.equals("/")){
-            res = num1 / num2;
+            res = this.num1 * this.num2;
+        } else {
+            res = this.num1 / this.num2;
         }
-        return String.format("%d %s %d = %d", this.num1, this.opcode, this.num2, res);
+
+//        switch (){}
+
+        String result = String.format("%d %s %d = %d", this.num1, this.opcode, this.num2, res);
+        return result;
     }
 
 }
--- java/com/example/demo/login/LoginApp.java
+++ java/com/example/demo/login/LoginApp.java
@@ -4,24 +4,32 @@ package com.example.demo.login;
  * fileName   : LoginApp.java
  * author     : choigeonil
  * date       : 2022-01-25
- * desc       : 아이디, 패스워드, 이름을 입력받아 출력하는 앱
+ * desc       : 아이디, 패스워드, 이름을 입력받아 로그인 결과를 출력하는 앱
  * ================================
  * DATE         AUTHOR        NOTE
  * ================================
  * 2022-01-24   choigeonil    최초 생성
+ * 2022-01-26   choigeonil    if절 작성, 성공실패 결과추가
  */
 public class LoginApp {
     static String LOGIN_APP = "로그인 앱";
     String id;
     String pw;
     String name;
+    static String PASSWORD = "abc";
 
     public String login(String id, String pw, String name){
         this.id = id;
         this.pw = pw;
         this.name = name;
 
-        return String.format("ID: %s, PW: %s, Name: %s", this.id, this.pw, this.name);
+        String res = "";
+        if (pw.equals("abc")){
+            res = String.format("%s 님의 비번 %s 가 맞습니다. 로그인 성공", this.name, this.pw);
+        } else
+            res = String.format("%s 님의 ID는 맞고, 비번 %s가 틀립니다. 로그인 실패", this.id, this.pw);
+
+        return res;
     }
 
 }
