136987b39c1b54b294f95f579e7eba3c57fc8136
nengcham <97512602+nengcham@users.noreply.github.com>
2022-02-02 20:29:09 +0900

    Update AlgoController.java

--- java/com/example/demo/controller/AlgoController.java
+++ java/com/example/demo/controller/AlgoController.java
@@ -46,6 +46,7 @@ public class AlgoController {
                     break;
                 default:
                     System.out.println("Wrong Number"); break;
+                    
             }
         }
     }
